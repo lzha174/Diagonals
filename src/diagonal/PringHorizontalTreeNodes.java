fdb6d5a025d5f274b6e2793e811da85ecf47dc2f
Oddo Zhang <oddo.zhang@navico.com>
2016-10-26 15:38:29 +1300

    threadedtree

--- src/diagonal/PringHorizontalTreeNodes.java
+++ src/diagonal/PringHorizontalTreeNodes.java
@@ -7,12 +7,15 @@ class Node{
 	int data;
 	Node left;
 	Node right;
+    boolean rightThread;
 	public Node(int data){
 		this.data = data;
 		this.left = null;
 		this.right =null;
+		rightThread = false;
 	}
 }
+
 
 public class PringHorizontalTreeNodes {
 	
