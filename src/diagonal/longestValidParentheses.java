8f2e5e78df7e824815f2339ada267baf957d3773
Oddo Zhang <oddo.zhang@navico.com>
2016-11-01 12:43:39 +1300

    a

---
+++ src/diagonal/longestValidParentheses.java
@@ -0,0 +1,39 @@
+package diagonal;
+
+import java.util.Stack;
+
+public class longestValidParentheses {
+
+	public static void main(String[] args) {
+		String s = "))(()";
+		// TODO Auto-generated method stub
+		Stack<int[]> stack = new Stack<int[]>();
+		int result = 0;
+	 
+		for(int i=0; i<=s.length()-1; i++){
+			char c = s.charAt(i);
+			if(c=='('){
+				int[] a = {i,0};
+				stack.push(a);
+			}else{
+				if(stack.empty()||stack.peek()[1]==1){
+					int[] a = {i,1};
+					stack.push(a);
+					// only push ) into stack if stack is empty or last element is )..s
+				}else{
+					stack.pop();
+					int currentLen=0;
+					if(stack.empty()){
+						currentLen = i+1;
+					}else{
+						currentLen = i-stack.peek()[0];
+					}
+					result = Math.max(result, currentLen);
+				}
+			}
+		}
+	 
+		result = result;
+	}
+
+}
